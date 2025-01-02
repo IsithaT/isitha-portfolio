0471bf75a76564d8ddf8bd515aab503abab75fd8
Isitha Tennakoon <82481046+IsithaT@users.noreply.github.com>
2025-01-01 17:48:02 -0800

    removed duplicate styles

--- portfolio/src/app/page.tsx
+++ portfolio/src/app/page.tsx
@@ -35,13 +35,12 @@ export default function Home() {
             <div className="min-w-fit min-h-fit gap-3 flex flex-col justify-between items-end p-5">
               <img className="grow object-cover w-[354px] rounded-2xl" src='https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fpre00.deviantart.net%2Fb31f%2Fth%2Fpre%2Ff%2F2017%2F085%2Fe%2F4%2Fpixel_art_profile_image_by_lonezi-db3oypd.png&f=1&nofb=1&ipt=dfdcff8b2e6174b3ee851f6feed56c4962a325b440840367c48e0bcb5989d209&ipo=images' alt="placeholder" />
               <button
-                className="w-full relative px-8 py-2 rounded-md isolation-auto z-10 border-2 border-red-700 
+                className="w-full relative px-8 py-2 isolation-auto z-10 border-2 border-red-700 
                 before:absolute before:w-full before:transition-all before:duration-700 before:hover:w-full 
                 hover:text-white before:-left-full before:hover:left-0 before:rounded-full before:bg-[#A12347] 
                 before:-z-10 before:aspect-square before:hover:scale-150 overflow-hidden before:hover:duration-700 
-                inline-flex items-center justify-center px-4 py-3 text-sm font-semibold text-black 
-                border border-gray-200 rounded-lg shadow-sm gap-x-2 disabled:opacity-50 
-                disabled:pointer-events-none transition-colors duration-700"
+                inline-flex items-center justify-center text-sm font-semibold text-black rounded-lg shadow-sm 
+                gap-x-2 disabled:opacity-50 disabled:pointer-events-none transition-colors duration-700"
               >
                 Resume
               </button>
